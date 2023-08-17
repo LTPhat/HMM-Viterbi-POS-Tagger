56eab0995f9892683b6ee11bbce8a94c972f644e
LTPhat <lamthanhphat0918398911@gmail.com>
2023-08-17 20:28:33 +0700

    train and test corpus

--- HMM/main.py
+++ HMM/main.py
@@ -7,6 +7,7 @@ from load import *
 from utils import *
 from hmm import HMM
 from viterbi import Viterbi
+from process_test_corpus import *
 
 
 training_corpus_list = ['./data/WSJ_02-21.pos']
@@ -58,13 +59,46 @@ def main():
     print("-------- PARSER ...")
     parser = init_argparse()
     args   = parser.parse_args()
-    print(args.training_corpus)
     if not check_valid_input(args=args):
         print("-------Received parser inputs failed--------")
         return 
     print("-------Received parser inputs completed--------")
+    print("Vocab txt:", args.vocab_txt)
+    print("Training corpus:", args.training_corpus)
+    print("Test corpus:", args.test_corpus)
+    print("Alpha:", args.alpha)
 
-   
+    print("-------START TRAINING--------")
+    # Preprocess input
+    vocab_txt = args.vocab_txt
+    vocab = get_index_vocab(vocab_txt=vocab_txt)
+    training_corpus = get_training_corpus(args.training_corpus)
+    test_words, label = load_test_corpus(args.test_corpus)
+    _, test_words = preprocess_list(vocab=vocab, test_words_list=test_words)
+    alpha = float(args.alpha)
+    # Define HMM class for training
+    hmm = HMM(vocab=vocab, training_corpus=training_corpus, alpha=alpha)
+    hmm._create_counts()
+    hmm._create_transition_matrix()
+    hmm._create_emission_matrix()
+    # Define Viterbi class for testing
+    viterbi = Viterbi(vocab=vocab, tag_counts=hmm.tag_counts, transition_matrix=hmm.transition_matrix,
+                      emission_matrix=hmm.emission_matrix, test_words=test_words, y=label)
+    print("Viterbi initialized ...")
+    best_probs_init, best_paths_init = viterbi._initialize()
+    # viterbi.save_data(best_probs_init, "./npy", "best_probs_init")
+    # viterbi.save_data(best_paths_init, "./npy", "best_paths_init")
+    print("Running viterbi forward ...")
+    best_probs, best_paths = viterbi._forward()
+    # viterbi.save_data(best_probs, "./npy", "best_probs")
+    # viterbi.save_data(best_paths, "./npy", "best_paths")
+    print("Completed viterbi forward ...")
+    print("Saved best_probs and best_paths")
+    print("Running viterbi backward ...")
+    pred = viterbi._backward()
+    print("Completed viterbi backward ...")
+    print("Accuracy on {} corpus with (Alpha = {}) is: {}".format(vocab_txt, alpha, viterbi._calculate_accuracy()))
 
+    
 if __name__ == "__main__":
     main()
