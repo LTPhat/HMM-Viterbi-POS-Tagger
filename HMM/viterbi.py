e79bf68b3a0a197a68e900f76241a5a63f716700
LTPhat <lamthanhphat0918398911@gmail.com>
2023-08-17 20:30:03 +0700

    viterbi algo

--- HMM/viterbi.py
+++ HMM/viterbi.py
@@ -6,6 +6,7 @@ import math
 from process_test_corpus import *
 import os
 import pickle
+import pandas as pd
 
 
 class Viterbi(object):
@@ -27,10 +28,10 @@ class Viterbi(object):
         states: List of all possible tags
         test_word: file of test words (.words) after remove true labels
         """
-        self.states = list(sorted(self.tag_counts.keys()))
+        self.states = sorted(self.tag_counts.keys())
         self.best_probs = np.zeros((len(self.tag_counts), len(self.test_words)))     
         self.best_paths = np.zeros((len(self.tag_counts), len(self.test_words)), dtype = int)
-        self.pred = [None] * len(self.test_words)
+        self.pred = [None] * (len(self.test_words) + 1)
         self.y = y
 
 
@@ -83,11 +84,11 @@ class Viterbi(object):
     
     
     def _backward(self):
-        n = len(self.test_words)
+        n = self.best_paths.shape[1]
         # Array z store the decision of each colummn
-        z = [None] * n
+        z = [None] * (n + 1)
         num_tags = len(self.tag_counts)
-        # Find largest probability of last column
+        # # Find largest probability of last column
         z[n - 1] = np.argmax(self.best_probs[:, n - 1])
         self.pred[n - 1] = self.states[z[n - 1]]
 
@@ -95,16 +96,16 @@ class Viterbi(object):
             pos_tag_for_word_i = z[i]
             z[i - 1] = self.best_paths[pos_tag_for_word_i, i]
             self.pred[i - 1] = self.states[z[i - 1]]
-        return self.pred
+        return self.pred[:-1]
     
 
     def _calculate_accuracy(self):
         """
         Calculate accuracy of self.pred and self.y
         """
-        assert len(self.pred) == len(self.y)
+        assert len(self.pred) == len(self.y) + 1 
         num_correct = 0
-        for pred, label in zip(self.pred, self.y):
+        for pred, label in zip(self.pred[:-1], self.y):
             if (pred == label):
                 num_correct += 1
         return num_correct / len(self.pred)
@@ -120,37 +121,41 @@ class Viterbi(object):
     
 if __name__ == "__main__":
     print("------------Running viterbi.py---------------")
-    vocab_txt="./data/vocab.txt"
+    vocab_txt="./data/hmm_vocab.txt"
     vocab = get_index_vocab(vocab_txt=vocab_txt)
     training_corpus = "./data/WSJ_02-21.pos"
     training_corpus = get_training_corpus(training_corpus)
-    test_corpus="./data/WSJ_23.pos"
+    test_corpus="./data/WSJ_24.pos"
     test_words, label = load_test_corpus(test_corpus)
     _, test_words = preprocess_list(vocab=vocab, test_words_list=test_words)
 
-    hmm = HMM(training_corpus=training_corpus, vocab=vocab, alpha=1)
+    hmm = HMM(training_corpus=training_corpus, vocab=vocab, alpha=0.001)
     hmm._create_counts()
     hmm._create_transition_matrix()
     hmm._create_emission_matrix()
 
-    # Define class
+    # # # Define class
     viterbi = Viterbi(vocab=vocab, tag_counts=hmm.tag_counts, transition_matrix=hmm.transition_matrix
                       ,emission_matrix=hmm.emission_matrix, test_words=test_words, y=label)
     
     best_probs_init, best_paths_init = viterbi._initialize()
     print("Viterbi initialized ...")
-    # viterbi.save_data(best_probs_init, "./npy", "best_probs_init")
-    # viterbi.save_data(best_paths_init, "./npy", "best_paths_init")
+    viterbi.save_data(best_probs_init, "./npy", "best_probs_init")
+    viterbi.save_data(best_paths_init, "./npy", "best_paths_init")
     print("Running viterbi forward ...")
     best_probs, best_paths = viterbi._forward()
+    viterbi.save_data(best_probs, "./npy", "best_probs")
+    viterbi.save_data(best_paths, "./npy", "best_paths")
     print("Completed viterbi forward ...")
-    # viterbi.save_data(best_probs, "./npy", "best_probs")
-    # viterbi.save_data(best_paths, "./npy", "best_paths")
     print("Saved best_probs and best_paths")
     print("Running viterbi backward ...")
     pred = viterbi._backward()
     print("Completed viterbi backward ...")
     print("Accuracy on test_words is: {}".format(viterbi._calculate_accuracy()))
-
+    print(len(pred))
+    print(len(viterbi.test_words))
+    print(test_words[-10:])
+    print(pred[-10:])
+    print(pred[-10:-1])
 
     
