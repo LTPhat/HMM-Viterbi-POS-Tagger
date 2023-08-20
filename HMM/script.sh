d2a2fea914c57e25f9a4e6afe75cefcd247d8093
LTPhat <lamthanhphat0918398911@gmail.com>
2023-08-20 17:19:06 +0700

    upload

--- HMM/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# You can adjust params then run bash script 
-
-python main.py --training_corpus './data/WSJ_02-21.pos' --vocab_txt './data/hmm_vocab.txt' --test_corpus './data/WSJ_24.pos' --alpha 0.001
-
-python predict_new.py --training_corpus "./data/WSJ_02-21.pos" --vocab_txt "./data/hmm_vocab.txt" --alpha 0.001 --stn "I am a student at school."
-
