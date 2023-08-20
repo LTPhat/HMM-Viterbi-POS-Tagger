e23e2684da4350c927d2279685cdda6699065dab
LTPhat <lamthanhphat0918398911@gmail.com>
2023-08-20 19:00:27 +0700

    bash shell

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@
 
 python main.py --training_corpus './data/WSJ_02-21.pos' --vocab_txt './data/hmm_vocab.txt' --test_corpus './data/WSJ_24.pos' --alpha 0.001
 
-python predict_new.py --training_corpus "./data/WSJ_02-21.pos" --vocab_txt "./data/hmm_vocab.txt" --alpha 0.001 --stn "I am a student at school."
+python predict_new.py --training_corpus "./data/WSJ_02-21.pos" --vocab_txt "./data/hmm_vocab.txt" --alpha 0.001 --stn "You just want attention. You dont want my heart."
 
